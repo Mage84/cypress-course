67966a7bdb1a2d530f3a8e9f87cab3a305b4b54e
Mage <chacha84de@hotmail.fr>
2025-08-26 14:00:18 +0200

    cypress 2nd tp

---
+++ cypress/e2e/lightbox.cy.js
@@ -0,0 +1,89 @@
+/// <reference types="cypress" />
+
+context('Connectors', () => {
+  beforeEach(() => {
+    cy.visit('../../lightbox/lightbox.html');
+  })
+
+  it('open and close lightbox on click', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('lightbox-opened').should('be.visible');
+    cy.get('body').click(0,0);
+    cy.getDataCy('lightbox-opened').should('not.visible');
+  })
+
+  it('add and delete like and update counter', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('like-count').should('have.text', '0')
+    cy.getDataCy('overlay-like-count').should('have.text', '0');
+    cy.getDataCy('like-btn').click();
+    cy.getDataCy('like-count').should('have.text', '1')
+    cy.getDataCy('overlay-like-count').should('have.text', '1');
+    cy.getDataCy('dislike-btn').click();
+    cy.getDataCy('like-count').should('have.text', '0')
+    cy.getDataCy('overlay-like-count').should('have.text', '0');
+  })
+
+  it('add comment and update counter', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('comment-count').should('have.text', '0')
+    cy.getDataCy('show-comment-btn').should('include.text', '0')
+    cy.getDataCy('comment-input').type('Cypress is awesome!');
+    cy.getDataCy('add-comment-btn').click()
+    cy.getDataCy('comment-body').should('have.text', 'Cypress is awesome!')
+    cy.getDataCy('comment-author').should('have.text', 'johndoe')
+    cy.getDataCy('comment-count').should('have.text', '1')
+    cy.getDataCy('show-comment-btn').should('include.text', '1')
+    cy.getDataCy('delete-comment-btn').click();
+    cy.getDataCy('comment-count').should('have.text', '0')
+    cy.getDataCy('show-comment-btn').should('include.text', '0')
+  })
+
+  it('can\'t add comment', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('add-comment-btn').should('be.disabled')
+  })
+
+  it('show and hide comment', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('comment-input').type('a');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('comment-body').should('be.visible');
+    cy.getDataCy('show-comment-btn').click();
+    cy.getDataCy('comment-body').should('not.visible');
+    cy.getDataCy('show-comment-btn').click();
+    cy.getDataCy('comment-body').should('be.visible');
+  })
+
+  it('have plural text', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('comment-input').type('a');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('show-comment-btn').should('include.text', 'comment')
+    cy.getDataCy('show-comment-btn').should('not.include.text', 'comments')
+    cy.getDataCy('comment-input').type('b');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('show-comment-btn').should('include.text', 'comments')
+    cy.getDataCy('delete-comment-btn').eq(1).click();
+    cy.getDataCy('show-comment-btn').should('include.text', 'comment')
+    cy.getDataCy('show-comment-btn').should('not.include.text', 'comments')
+  })
+
+  it('delete right comment', () => {
+    cy.getDataCy('lightbox').click();
+    cy.getDataCy('comment-input').type('a');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('comment-input').type('b');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('comment-input').type('c');
+    cy.getDataCy('add-comment-btn').click();
+    cy.getDataCy('comment-body').should('have.length', 3)
+    cy.getDataCy('comment-body').eq(0).should('have.text', 'a')
+    cy.getDataCy('comment-body').eq(1).should('have.text', 'b')
+    cy.getDataCy('comment-body').eq(2).should('have.text', 'c')
+    cy.getDataCy('delete-comment-btn').eq(1).click();
+    cy.getDataCy('comment-body').should('have.length', 2)
+    cy.getDataCy('comment-body').eq(0).should('have.text', 'a')
+    cy.getDataCy('comment-body').eq(1).should('have.text', 'c')
+  })
+})
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,8 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+
+Cypress.Commands.add('getDataCy', (id) => {
+  return cy.get(`[data-cy=${id}]`)
+})
